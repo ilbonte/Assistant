6ede8626923c3d033ee5bb3a804a362b0e73dfab
Davide Bontempelli <davide.bonte@gmail.com>
2017-01-22 21:14:19 +0100

    add basic structure

--- bot.js
+++ bot.js
@@ -1,10 +1,26 @@
-'use strict';
+"use strict";
 
 var botBuilder = require('claudia-bot-builder');
+var parsedRequest = {};
+module.exports = botBuilder(function (request) {
+  var response = "-";
+  parsedRequest = request;
+  if (isBotCommand()) {
+    var command = request.text.substring(1);
+    switch (command) {
+      case "saluta":
+        response = "ciao";
+        break;
+      default:
+        response = "comando sconoscito";
+    }
+  }
 
-module.exports = botBuilder(function (request, originalApiRequest) {
-  var prettyResponse = 'Here is your request: \n ' + JSON.stringify(request, null, 1);
-  return prettyResponse;
+  return response;
 }, {
   platforms: ['telegram']
 });
+
+function isBotCommand() {
+  return parsedRequest.originalRequest.message.entities[0].type === "bot_command";
+}
--- src/bot.source.js
+++ src/bot.source.js
@@ -1,8 +1,24 @@
 const botBuilder = require('claudia-bot-builder')
+var parsedRequest = {}
+module.exports = botBuilder((request) => {
+  let response = "-"
+  parsedRequest = request
+  if (isBotCommand()) {
+    const command = request.text.substring(1)
+    switch (command) {
+      case "saluta":
+        response = "ciao"
+        break;
+      default:
+        response = "comando sconoscito"
+    }
+  }
 
-module.exports = botBuilder((request, originalApiRequest) => {
-  const prettyResponse = `Here is your request: \n ${JSON.stringify(request,null, 1)}`
-  return prettyResponse
-},{
+  return response
+}, {
   platforms: ['telegram']
 })
+
+function isBotCommand() {
+  return parsedRequest.originalRequest.message.entities[0].type==="bot_command"
+}
