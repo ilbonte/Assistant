272735a349b9248a2313088bb49df4a7737944a1
Davide Bontempelli <davide.bonte@gmail.com>
2017-01-29 20:10:56 +0100

    add GotAdapter

---
+++ test/GotAdapterTest.js
@@ -0,0 +1,19 @@
+let GotAdapter = require('../lib/GotAdapter')
+const {equal, deepEqual} = require('assert')
+const {test} = require('m.test')
+const expectedBikes = require('./fixtures/fullBike.json')
+
+test('can get data', function (done) {
+  let gotAdapter = new GotAdapter()
+  gotAdapter.get('https://os.smartcommunitylab.it/core.mobility/bikesharing/trento').then(function (result) {
+    var bikes = result.body;
+    equal(expectedBikes.length, bikes.length)
+    done()
+  }).catch(handle)
+})
+
+
+
+function handle() {
+  console.log("error")
+}
